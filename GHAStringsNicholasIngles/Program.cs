eb44185fec5adb22f387f1c5d089b7676844376c
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-10 00:13:13 -0800

    Add project files.

---
+++ GHAStringsNicholasIngles/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string myFirstName = "William";
+        string myLastName = "Cram";
+        string myFullName = myFirstName + " " + myLastName + " is my professor";
+        Console.WriteLine(myFullName);
+    } // End of Main
+}
